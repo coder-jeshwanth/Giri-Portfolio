c2eb2068ab6196e9d4555bdb8d7b0b932943474e
jeshy <24mcaa32@kristujayanti.com>
2025-12-14 13:27:40 +0530

    changes made by jeshy

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = themeToggle.querySelector('i');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('light-mode');
+  
+  if (body.classList.contains('light-mode')) {
+    themeIcon.classList.remove('fa-sun');
+    themeIcon.classList.add('fa-moon');
+  } else {
+    themeIcon.classList.remove('fa-moon');
+    themeIcon.classList.add('fa-sun');
+  }
+});
